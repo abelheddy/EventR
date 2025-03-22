c99c2271d125c9eab036042f585680d056668976
abelhecks <abelmicz@hotmail.com>
2025-03-21 22:43:08 -0600

    MainPage 2.0
    se modifico la pantalla principal y se cambio los colores a los del logo

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -3,22 +3,35 @@ import './css/MainPage.css';
 
 const MainPage = () => {
     return (
-        <nav>
-            <input type="checkbox" id="check" />
-            <label htmlFor="check" className="checkbtn">
-                <i className="fas fa-bars"></i>
-            </label>
-            <a href="#" className="enlace">
-                <img src="logo.png" alt="" className="logo" />
-            </a>
-            <ul>
-                <li><a className="active" href="#">Inicio</a></li>
-                <li><a href="#">Nosotros</a></li>
-                <li><a href="#">Servicios</a></li>
-                <li><a href="#">Portafolio</a></li>
-                <li><a href="#">Contacto</a></li>
-            </ul>
-        </nav>
+        <>
+            <nav>
+                <input type="checkbox" id="check" />
+                <label htmlFor="check" className="checkbtn">
+                    <i className="fas fa-bars"></i> {/* Ícono de hamburguesa */}
+                </label>
+                <a href="#" className="enlace">
+                    <img src="logo.png" alt="Logo" className="logo" /> {/* Logo */}
+                </a>
+                <ul className="nav-buttons">
+                    <li><a className="active" href="#">Inicio</a></li>
+                    <li><a href="#">Eventos</a></li>
+                    <li><a href="#">Acerca de</a></li>
+                    <li><a href="#">Contacto</a></li>
+                </ul>
+                <ul className="auth-buttons">
+                    <li><a href="/login">Login</a></li>
+                    <li><a href="/register">Register</a></li>
+                </ul>
+            </nav>
+
+            {/* Contenido principal */}
+            <section>
+                <div className="content">
+                    <div className="left-section">Sección Izquierda</div>
+                    <div className="right-section">Sección Derecha</div>
+                </div>
+            </section>
+        </>
     );
 };
 
