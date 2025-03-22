f3fe2ff5a518bb16418b6d6780bfb0697d8d0450
abelhecks <abelmicz@hotmail.com>
2025-03-22 11:45:02 -0600

    carrusel
    se agrego codigo nuevo al carrcel y se agregaron las imagenes

--- frontend/src/components/ImageCarousel.jsx
+++ frontend/src/components/ImageCarousel.jsx
@@ -5,10 +5,10 @@ const ImageCarousel = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const images = [
-    '/src/assets/images/image1.jpg', // Ruta de la primera imagen
-    '/src/assets/images/image2.jpg', // Ruta de la segunda imagen
-    '/src/assets/images/image3.jpg', // Ruta de la tercera imagen
-    '/src/assets/images/image4.jpg', // Ruta de la cuarta imagen
+    '/src/assets/images/event1.jpg', // Ruta de la primera imagen
+    '/src/assets/images/event2.jpg', // Ruta de la segunda imagen
+    '/src/assets/images/event3.jpg', // Ruta de la tercera imagen
+    '/src/assets/images/event4.jpg', // Ruta de la cuarta imagen
   ];
 
   const nextImage = () => {
@@ -22,10 +22,15 @@ const ImageCarousel = () => {
   return (
     <div className="carousel">
       <button className="carousel-button prev" onClick={prevImage}>&#10094;</button>
-      <img src={images[currentIndex]} alt={`Slide ${currentIndex}`} className="carousel-image" />
+      <img
+        src={images[currentIndex]}
+        alt={`Slide ${currentIndex}`}
+        className="carousel-image"
+      />
       <button className="carousel-button next" onClick={nextImage}>&#10095;</button>
     </div>
   );
 };
+
 
 export default ImageCarousel;
